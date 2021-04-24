a392ded4dd899d6493bd680d601d560208ca9b45
Bharat singh shekhawat <58418023+bharatrajput2409@users.noreply.github.com>
2021-04-24 16:07:13 +0530

    Update settings.py

--- BlogBackend/settings.py
+++ BlogBackend/settings.py
@@ -128,10 +128,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://webclub.nitk.ac.in'
-]
+CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
